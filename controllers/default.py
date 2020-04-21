6fbe4ad7004539cec8034f7389dc180a20a383f9
mydentalplan123 <imtiazbengali@gmail.com>
2020-04-22 03:07:49 +0530

    added comment line

--- controllers/default.py
+++ controllers/default.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
 # checking for git 
+#
 from gluon import current
 db = current.globalenv['db']
 
